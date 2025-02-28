9e71caf524061ae283c26b4791385484d57a7d4e
darko <darkomilanovski45@gmail.com>
2025-02-28 16:34:05 +0100

    Add project files.

---
+++ FirstHomeworkCSharp/Program.cs
@@ -0,0 +1,53 @@
+﻿
+#region first exercise
+int branches = 12;
+int applesPerBranch = 8;
+int basketCapacity = 5;
+
+Console.Write("Enter the number of trees: ");
+int numberOfTrees = int.Parse(Console.ReadLine());
+
+int totalApplesPerTree = branches * applesPerBranch;
+
+int totalApples = totalApplesPerTree * numberOfTrees;
+
+int basketsRequired = totalApples / basketCapacity;
+
+Console.WriteLine("Baskets required for:" + numberOfTrees + " trees is:" + basketsRequired);
+
+#endregion
+
+#region second exercise
+Console.WriteLine("Enter the first number:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int b = int.Parse(Console.ReadLine());
+if(a % 2 == 0)
+{
+    Console.WriteLine("The number " + a + " is even");
+}
+else
+{
+    Console.WriteLine("The number " + b + " is odd");
+}
+if(b % 2 == 0)
+{
+    Console.WriteLine("The number " + b + " is even");
+}
+else
+{
+    Console.WriteLine("The number " + b + " is odd");
+}
+if(a > b)
+{
+    Console.WriteLine("The larger number from the two is " + a);
+}
+else if(b > a)
+{
+    Console.WriteLine("The larger number from the two is " + b);
+}
+else if(a == b)
+{
+    Console.WriteLine("The numbers have the same value");
+}
+#endregion
---
+++ Homeworks/Program.cs
@@ -0,0 +1,40 @@
+﻿
+#region first task
+Console.Write("Enter the First number: ");
+decimal firstNumber = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Enter the Second number: ");
+decimal secondNumber = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Enter the Operation: ");
+string operation = Console.ReadLine();
+
+decimal result = 0;
+
+switch (operation)
+{
+    case "+":
+        result = firstNumber + secondNumber;
+        break;
+    case "-":
+        result = firstNumber - secondNumber;
+        break;
+    case "*":
+        result = firstNumber * secondNumber;
+        break;
+    case "/":
+        if (firstNumber == 0  || secondNumber == 0)
+        {
+            Console.WriteLine(" Division by zero is not allowed.");
+            return;
+        }
+        result = firstNumber / secondNumber;
+        break;
+    default:
+        Console.WriteLine("You have entered an invalid operation.");
+        break;
+}
+
+Console.WriteLine("The result is:" + result);
+    
+#endregion
