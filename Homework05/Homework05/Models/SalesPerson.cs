6c8c230eacfe6703a7d869076a7db5c4c12a1815
darko <darkomilanovski45@gmail.com>
2025-03-22 20:53:33 +0100

    homework 5 added

---
+++ Homework05/Homework05/Models/SalesPerson.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Homework05.Models
+{
+   public class SalesPerson : Employee
+    {
+        public SalesPerson(string firstName, string lastName, double salary) : base(firstName, lastName, salary)
+        { }
+    }
+}
---
+++ Qinshift.Inheritance/Qinshift.Class07/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Qinshift.Class07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
